7220023833f92b14265e1c7493ef7cb3c170240e
Sumeyye Dagdeviren <sumeyyedagdevirozmen@gmail.com>
2022-06-09 17:26:12 +0100

    first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class RESTAPITest {
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi- <artifactId>maven-surefire-plugin</artifactId>\r\n"
+				+ "          <version>3.0.0-M5</version>");
+	}
+
+}
