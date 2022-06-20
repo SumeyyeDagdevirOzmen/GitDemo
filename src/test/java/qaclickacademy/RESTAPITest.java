61b8c1679d9e88e3cfee549a93518942bbaefbdf
Sumeyye Dagdeviren <sumeyyedagdevirozmen@gmail.com>
2022-06-20 11:13:35 +0100

    Architect Asian development

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -7,10 +7,27 @@ public class RESTAPITest {
 	public void postJira()
 	{
 		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		
+		
+		
+		
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		
 	}
 	@Test
 	public void deleteTwitter()
 	{
 		System.out.println("deleteTwitter");
+	}
+	
+	@Test
+	public void deleteTwitter12()
+	{
+		System.out.println("deleteTdwitter");
 	}
 }
--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -3,16 +3,25 @@ package qaclickacademy;
 import org.junit.Test;
 
 public class SeleniumTest {
+	
+	//testng xml file from Maven
+	//how to execute all test cases from Test folder -mvn test
 	@Test
 	public void BrowserAutomation()
 	{
 		System.out.println("BrowserAutomation");
+		System.out.println("Web Automation");
 	}
 	@Test
 	public void elementsUi()
 	{
-		System.out.println("elementsUi- <artifactId>maven-surefire-plugin</artifactId>\r\n"
-				+ "          <version>3.0.0-M5</version>");
+		System.out.println("elementsUi");
 	}
+	@Test
+	public void elementsUi1()
+	{
+		System.out.println("elementsUi");
+	}
+
 
 }
